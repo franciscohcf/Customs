f9d0c249448e84088fab3f0075b6b3d522db83cd
Francisco Holanda <franciscocavalcantefilho@gmail.com>
2021-05-11 09:35:14 -0300

    Projeto finalizado

--- src/main/java/aplication/App.java
+++ src/main/java/aplication/App.java
@@ -1,4 +1,66 @@
 package aplication;
 
+import entities.ImportedProduct;
+import entities.Product;
+import entities.UsedProduct;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
 public class App {
+    public static void main(String[] args) throws ParseException {
+
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+
+        List<Product> list = new ArrayList<>();
+
+        System.out.println("Enter the number of products: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("Product #" + i + " data:");
+            System.out.println("Common, used or imported (c/u/i)? ");
+            char ch = sc.next().charAt(0);
+            if (ch == 'c') {
+                System.out.println("Name: ");
+                sc.nextLine();
+                String name = sc.nextLine();
+                System.out.println("Price: ");
+                double price = sc.nextDouble();
+                list.add(new Product(name, price));
+            }
+            if (ch == 'u') {
+                System.out.println("Name: ");
+                sc.nextLine();
+                String name = sc.nextLine();
+                System.out.println("Price: ");
+                double price = sc.nextDouble();
+                System.out.println("Manufacture date (DD/MM/YYYY)");
+                Date manufactureDate = sdf.parse(sc.next());
+                list.add(new UsedProduct(name, price,manufactureDate));
+            }
+            if (ch == 'i') {
+                System.out.println("Name: ");
+                sc.nextLine();
+                String name = sc.nextLine();
+                System.out.println("Price: ");
+                double price = sc.nextDouble();
+                System.out.println("Customs fee: ");
+                double customsFee = sc.nextDouble();
+                list.add(new ImportedProduct(name, price,customsFee));
+            }
+        }
+        System.out.println();
+
+        System.out.println("PRICE TAGS");
+
+        for (Product p:list) {
+            System.out.println(p.priceTag());;
+        }
+
+        sc.close();
+    }
 }
--- src/main/java/entities/UsedProduct.java
+++ src/main/java/entities/UsedProduct.java
@@ -1,4 +1,26 @@
 package entities;
 
-public class UsedProduct {
+import java.util.Date;
+
+public class UsedProduct extends Product{
+
+    protected Date manufactureDate;
+
+    public UsedProduct() {
+        super();
+    }
+
+    public UsedProduct(String name, Double price, Date manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+    public Date getManufactureDate() {
+        return manufactureDate;
+    }
+
+    @Override
+    public String priceTag() {
+        return super.getName() + " (used) " + " $ " + super.getPrice() + "(Manufacture date: " + " $ " + this.getManufactureDate() + ")";
+    }
 }
